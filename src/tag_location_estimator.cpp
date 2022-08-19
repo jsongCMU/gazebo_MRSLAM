b5f5642d8432dcaaf15780aa44f82efa08555c73
JSongLaptop <jaekyuns@andrew.cmu.edu>
2022-08-19 12:51:44 -0700

    Added location estimation for multiple tags from multiple agents; integrated into RVIZ

--- src/tag_location_estimator.cpp
+++ src/tag_location_estimator.cpp
@@ -12,7 +12,8 @@ struct tag_coord
   bool is_new = false;
 };
 
-const uint num_robots = 0;
+const std::string fixed_frame = "map";
+const uint num_robots = 2;
 const std::string robot_prefix = "agent";
 const uint num_tags = 14;
 const std::string tag_prefix = "tag_";
@@ -24,47 +25,55 @@ int main(int argc, char** argv){
   tf::TransformListener listener;
   ros::Duration(3.0).sleep();
   ros::Rate rate(10.0);
-  // Keeping track of 
+  
   while (nh.ok()){
-    // Get transform
     tf::StampedTransform transform;
-    try{
-      listener.lookupTransform("/map", "/tag_0/agent1",  
-                               ros::Time(0), transform);
-      // Generate and publish message
-      visualization_msgs::Marker m;
-      m.header.frame_id = "map";
-      // label
-      m.ns = "tag";
-      m.id = 0;
-      // shape
-      m.type = 2; // Sphere
-      // what to do
-      m.action = 0; // Add/modify
-      // pose
-      m.pose.position.x = transform.getOrigin().x();
-      m.pose.position.y = transform.getOrigin().y();
-      m.pose.position.z = transform.getOrigin().z();
-      m.pose.orientation.x = m.pose.orientation.y = m.pose.orientation.z = 0;
-      m.pose.orientation.w = 1;
-      // Size
-      m.scale.x = 1;
-      m.scale.y = 1;
-      m.scale.z = 1;
-      // Color
-      m.color.r = 1;
-      m.color.g = 1;
-      m.color.b = 1;
-      m.color.a = 1;
-      // Lifetime
-      m.lifetime = ros::Duration(0); // forever
-      pub_mk.publish(m);
-
-      rate.sleep();
-    }
-    catch (tf::TransformException ex){
-      ROS_ERROR("[tag_location_estimator] %s",ex.what());
-      ros::Duration(1.0).sleep();
+    bool transform_found = true;
+    // Try all possible transform combinations of tags and agents
+    for(uint robot_idx = 0; robot_idx < num_robots; robot_idx++){
+      for(uint tag_idx = 0; tag_idx < num_tags; tag_idx++){
+        std::string cur_robot_name = robot_prefix + std::to_string(robot_idx);
+        std::string cur_tag_name = tag_prefix + std::to_string(tag_idx);
+        try{
+          listener.lookupTransform("/" + fixed_frame, "/" + cur_tag_name + "/" + cur_robot_name, ros::Time(0), transform);
+          transform_found = true;
+        }
+        catch (tf::TransformException ex){
+          transform_found = false;
+        }
+        if(transform_found){
+          // Found transform; generate and publish message
+          visualization_msgs::Marker m;
+          m.header.frame_id = fixed_frame;
+          // label
+          m.ns = tag_prefix;
+          m.id = tag_idx;
+          // shape
+          m.type = 2; // Sphere
+          // what to do
+          m.action = 0; // Add/modify
+          // pose
+          m.pose.position.x = transform.getOrigin().x();
+          m.pose.position.y = transform.getOrigin().y();
+          m.pose.position.z = transform.getOrigin().z();
+          m.pose.orientation.x = m.pose.orientation.y = m.pose.orientation.z = 0;
+          m.pose.orientation.w = 1;
+          // size
+          m.scale.x = 0.3;
+          m.scale.y = 0.3;
+          m.scale.z = 0.3;
+          // color
+          m.color.r = 0.2;
+          m.color.g = 0.2;
+          m.color.b = 1;
+          m.color.a = 1;
+          // lifetime
+          m.lifetime = ros::Duration(0); // forever
+          // publish
+          pub_mk.publish(m);
+          rate.sleep();
+        }
+      }
     }
   }
   return 0;
