ddd9c1c8148679c83864a22a0a555a442a3449bf
jsongCMU <jaekyuns@andrew.cmu.edu>
2022-09-25 15:17:12 -0400

    Converted tf to tf2

--- src/apriltag_publisher.cpp
+++ src/apriltag_publisher.cpp
@@ -2,11 +2,13 @@
 
 #include <ros/ros.h>
 #include <tf/transform_listener.h>
+#include <tf2_ros/transform_listener.h>
+#include <geometry_msgs/TransformStamped.h>
 #include "apriltag_ros/AprilTagDetectionArray.h"
 #include "apriltag_ros/AprilTagDetection.h"
 
-const std::string base_frame = "/agent1/camera";
-const std::string tag_frame = "/tag_1/agent1";
+const std::string base_frame = "agent1/camera";
+const std::string tag_frame = "tag_1/agent1";
 const std::string topic_name = "/robosar_agent_bringup_node/agent1/feedback/apriltag";
 
 int main(int argc, char** argv){
@@ -16,30 +18,34 @@ int main(int argc, char** argv){
   tf::TransformListener tf_listener;
   ros::Duration(3.0).sleep();
   ros::Rate rate(10.0);
+
+  tf2_ros::Buffer tfBuffer;
+  tf2_ros::TransformListener tfListener(tfBuffer);
   
   while (nh.ok()){
-    tf::StampedTransform transform;
+    // Get transform
+    geometry_msgs::TransformStamped transformStamped;
     bool transform_found = true;
-    // Try all possible transform combinations of tags and agents
     try{
-      tf_listener.lookupTransform(base_frame, tag_frame, ros::Time(0), transform);
+      transformStamped = tfBuffer.lookupTransform(base_frame, tag_frame, ros::Time(0));
       transform_found = true;
+      std::cout << "Transform found!\n";
     }
-    catch (tf::TransformException ex){
+    catch (tf2::TransformException &ex) {
       transform_found = false;
     }
+    // If transform found
     if(transform_found){
       // Found transform; generate and publish message
       apriltag_ros::AprilTagDetectionArray msg;
-      msg.header.stamp = transform.stamp_;
-      msg.header.frame_id = base_frame;
+      msg.header = transformStamped.header;
 
       apriltag_ros::AprilTagDetection detection;
       detection.id.push_back(1);
       detection.size.push_back(0);
-      detection.pose.pose.pose.position.x = transform.getOrigin().getX();
-      detection.pose.pose.pose.position.y = transform.getOrigin().getY();
-      detection.pose.pose.pose.position.z = transform.getOrigin().getZ();
+      detection.pose.pose.pose.position.x = transformStamped.transform.translation.x;
+      detection.pose.pose.pose.position.y = transformStamped.transform.translation.y;
+      detection.pose.pose.pose.position.z = transformStamped.transform.translation.z;
 
       msg.detections.push_back(detection);
       
